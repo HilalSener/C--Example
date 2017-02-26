c232d2a235aea793b9f422953dbc4cdd6c950939
HilalSener <crescent.forest@hotmail.com>
2017-02-26 14:38:45 +0300

    SinifGecme project added

---
+++ ConsoleApplications/SinifGecme/SinifGecme/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinifGecme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //alınan iki notun ortalamasını hesaplayıp 50 den büyük ve eşitse geçti, değilse kaldı. 90 - 100 arası A ile geçti. 80-90 B. 80-70 C. 70-60 D. 60-50 E gibi.
+        }
+    }
+}
