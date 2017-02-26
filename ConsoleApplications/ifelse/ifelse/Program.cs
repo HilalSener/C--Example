a23c55eba6e103b399cfbd0a639a13c7ba9b8d9d
HilalSener <crescent.forest@hotmail.com>
2017-02-26 11:31:04 +0300

    ifelse project added.

---
+++ ConsoleApplications/ifelse/ifelse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen iki sayıdan büyük olanı ekranda Büyük: sayı şeklinde gösteren uygulamayı yazınız.
+            Console.Write("1.Sayıyı giriniz: ");
+            int Sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2.Sayıyı giriniz: ");
+            int Sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (Sayi1 > Sayi2)
+                Console.WriteLine(Sayi1 + " > " + Sayi2);
+            else
+                Console.WriteLine(Sayi2 + " > " + Sayi1);
+
+            Console.ReadKey();
+        }
+    }
+}
