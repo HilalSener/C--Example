4920ba77e4136d15e01c1f98d610d99ddd83de61
HilalSener <crescent.forest@hotmail.com>
2017-02-26 14:33:23 +0300

    DortIslem project added.

---
+++ ConsoleApplications/DortIslem/DortIslem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DortIslem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen iki sayıya menüden seçilen işlemi uygulayıp sonucu ekrana yazdıran uygulamayı yazınız.
+        }
+    }
+}
