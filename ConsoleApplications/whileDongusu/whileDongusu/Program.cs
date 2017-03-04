ca48d021bc3106caf0a7a9cc761bd1a624b11a52
HilalSener <crescent.forest@hotmail.com>
2017-03-04 11:47:49 +0300

    whileDongusu project added.

---
+++ ConsoleApplications/whileDongusu/whileDongusu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ekrana 5 defa Merhaba .Net yazdıran uygulama
+            //int i = 0;
+            //for (; i < 5 ;)
+            //{
+            //    Console.WriteLine("Merhaba .Net");
+            //    i++;
+            //}
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine("Merhaba .Net");
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
