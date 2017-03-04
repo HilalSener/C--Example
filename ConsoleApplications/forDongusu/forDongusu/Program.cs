6476fef58d5a634ad5597a2d2ed4b03ecf85288a
HilalSener <crescent.forest@hotmail.com>
2017-03-04 11:28:00 +0300

    forDongusu project added.

---
+++ ConsoleApplications/forDongusu/forDongusu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ekrana alt alta 5 defa Merhaba .Net yazısını görüntüleyen uygulamayı yazınız.
+            //for (int i = 0; i < 5; i++)
+            //    Console.WriteLine("Merhaba .Net");
+
+            //1 - 10 sayıları alt alta gösteren uygulama
+            for (int i=1; i<=10; i++)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
