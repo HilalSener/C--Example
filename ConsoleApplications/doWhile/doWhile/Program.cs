c7770152583c5b0a1eb2b2e90e454276b5ef96fe
HilalSener <crescent.forest@hotmail.com>
2017-03-04 13:29:17 +0300

    New solution added into doWhile project

--- ConsoleApplications/doWhile/doWhile/Program.cs
+++ ConsoleApplications/doWhile/doWhile/Program.cs
@@ -13,6 +13,7 @@ namespace doWhile
             //doWhile döngülerinin en önemli özelliği, önce işlemler bir defa çalıştırılır. Daha sonra şart kontrol edilir.Bu nedenle döngü en az bir defa çalışmış olur.
 
             //şifre olarak 9999 girilinceye kadar şifre istemeye devam eden uygulamayı yazınız. 
+            //Kullanıcı 3 defa şifreyi yanlış girerse hakkınız doldu şeklinde uyararak programı sonlandıralım.
             string sifre;
             int sayac = 0;
             do
@@ -20,6 +21,12 @@ namespace doWhile
                 sayac++;
                 Console.Write("Şifrenizi giriniz: ");
                 sifre = Console.ReadLine();
+                if (sayac == 3)
+                {
+                    Console.WriteLine("Hakkınız doldu!");
+                    Console.ReadKey();
+                    return;
+                }
             } while (sifre != "9999");
             Console.WriteLine("Hoşgeldiniz...");
 
