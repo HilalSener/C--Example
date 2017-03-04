b94869292b89daa8b56c57c814f1a3c5b4a8dae3
HilalSener <crescent.forest@hotmail.com>
2017-03-04 12:03:37 +0300

    New solution added into whileDongusu project

--- ConsoleApplications/whileDongusu/whileDongusu/Program.cs
+++ ConsoleApplications/whileDongusu/whileDongusu/Program.cs
@@ -18,12 +18,35 @@ namespace whileDongusu
             //    i++;
             //}
 
-            int i = 0;
-            while (i < 5)
+            //int i = 0;
+            //while (i < 5)
+            //{
+            //    Console.WriteLine("Merhaba .Net");
+            //    i++;
+            //}
+
+            //başlangıç ve bitiş değerleri klavyeden girildiğindei girilen sayılar arasındaki değerleri ekranda alt alta gösteren uygulamayı yazınız.(Hem for hem de while ile)
+            Console.Write("Başlangıç değerini giriniz: ");
+            int deger1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bitiş değerini giriniz: ");
+            int deger2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Artış değerini giriniz: ");
+            int artis = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = deger1; i <= deger2; i = i + artis)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int i = deger1;
+            while(i<=deger2)
             {
-                Console.WriteLine("Merhaba .Net");
-                i++;
+                Console.WriteLine(i);
+                i += artis;
             }
+
             Console.ReadKey();
         }
     }
