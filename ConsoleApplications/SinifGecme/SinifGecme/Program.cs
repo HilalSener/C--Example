cfd1b451a3befc6de8500ba21540f7e4a544a4dc
HilalSener <crescent.forest@hotmail.com>
2017-03-04 10:32:18 +0300

    New solution added into SinifGecme project.

--- ConsoleApplications/SinifGecme/SinifGecme/Program.cs
+++ ConsoleApplications/SinifGecme/SinifGecme/Program.cs
@@ -14,30 +14,30 @@ namespace SinifGecme
             basla:
             Console.Write("1.Notu giriniz: ");
             float Not1 = Convert.ToSingle(Console.ReadLine());
-            if (Not1 > 100)
-            {
-                Console.WriteLine("Not1 100 den büyük olamaz!");
-                goto basla;
-            }
-            else if(Not1 < 0)
-            {
-                Console.WriteLine("Not1 0 dan küçük olamaz!");
-                goto basla;
-            }
+            //if (Not1 > 100)
+            //{
+            //    Console.WriteLine("Not1 100 den büyük olamaz!");
+            //    goto basla;
+            //}
+            //else if(Not1 < 0)
+            //{
+            //    Console.WriteLine("Not1 0 dan küçük olamaz!");
+            //    goto basla;
+            //}
 
-            NotGiris2:
+            //NotGiris2:
             Console.Write("2.Notu giriniz: ");
             float Not2 = Convert.ToSingle(Console.ReadLine());
-            if (Not2 > 100)
-            {
-                Console.WriteLine("Not2 100 den büyük olamaz!");
-                goto NotGiris2;
-            }
-            else if (Not2 < 0)
-            {
-                Console.WriteLine("Not2 0 dan küçük olamaz!");
-                goto NotGiris2;
-            }
+            //if (Not2 > 100)
+            //{
+            //    Console.WriteLine("Not2 100 den büyük olamaz!");
+            //    goto NotGiris2;
+            //}
+            //else if (Not2 < 0)
+            //{
+            //    Console.WriteLine("Not2 0 dan küçük olamaz!");
+            //    goto NotGiris2;
+            //}
 
             float Ortalama = (Not1 + Not2) / 2f;
 
@@ -46,19 +46,27 @@ namespace SinifGecme
             //else
             //    Console.WriteLine("Kaldı");
 
-            if (Ortalama >= 90)
-                Console.WriteLine("{0}, A ile geçti!", Ortalama);
-            else if (Ortalama >= 80)
-                Console.WriteLine("{0}, B ile geçti!", Ortalama);
-            else if (Ortalama >= 70)
-                Console.WriteLine("{0}, C ile geçti!", Ortalama);
-            else if (Ortalama >= 60)
-                Console.WriteLine("{0}, D ile geçti", Ortalama);
-            else if (Ortalama >= 50)
-                Console.WriteLine("{0}, E ile geçti", Ortalama);
-            else if (Ortalama < 50)
-                Console.WriteLine("{0}, F ile Kaldı", Ortalama);
-
+            if (Not1 > 0 && Not1 < 100 && Not2 > 0 && Not2 <100)
+            {
+                if (Ortalama >= 90)
+                    Console.WriteLine("{0}, A ile geçti!", Ortalama);
+                else if (Ortalama >= 80)
+                    Console.WriteLine("{0}, B ile geçti!", Ortalama);
+                else if (Ortalama >= 70)
+                    Console.WriteLine("{0}, C ile geçti!", Ortalama);
+                else if (Ortalama >= 60)
+                    Console.WriteLine("{0}, D ile geçti", Ortalama);
+                else if (Ortalama >= 50)
+                    Console.WriteLine("{0}, E ile geçti", Ortalama);
+                else if (Ortalama < 50)
+                    Console.WriteLine("{0}, F ile Kaldı", Ortalama);
+            }
+            else
+            {
+                Console.WriteLine("Notlar 0 - 100 arasında girilmelidir!");
+                goto basla;
+            }
+           
             Console.WriteLine("Devam etmek istiyor musunuz? (E/H)");
             string Cevap = Console.ReadLine();
 
