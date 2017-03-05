2f5612912523ee36677933ef6c95f1b6a802f5b6
HilalSener <crescent.forest@hotmail.com>
2017-03-05 10:03:16 +0300

    Toplama project added.

---
+++ WindowsFormApplications/Toplama/Toplama/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Toplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnToplama = new System.Windows.Forms.Button();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı2:";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(116, 26);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            this.txtSayi1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(116, 56);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            this.txtSayi2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnToplama
+            // 
+            this.btnToplama.Location = new System.Drawing.Point(116, 93);
+            this.btnToplama.Name = "btnToplama";
+            this.btnToplama.Size = new System.Drawing.Size(75, 23);
+            this.btnToplama.TabIndex = 4;
+            this.btnToplama.Text = "Topla";
+            this.btnToplama.UseVisualStyleBackColor = true;
+            this.btnToplama.Click += new System.EventHandler(this.btnToplama_Click);
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Location = new System.Drawing.Point(116, 131);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.ReadOnly = true;
+            this.txtSonuc.Size = new System.Drawing.Size(75, 20);
+            this.txtSonuc.TabIndex = 5;
+            this.txtSonuc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnToplama);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnToplama;
+        private System.Windows.Forms.TextBox txtSonuc;
+    }
+}
+
---
+++ WindowsFormApplications/Toplama/Toplama/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToplama_Click(object sender, EventArgs e)
+        {
+            //int Sayi1 = Convert.ToInt32(txtSayi1.Text);
+            //int Sayi2 = Convert.ToInt32(txtSayi2.Text);
+
+            //int Toplama = Sayi1 + Sayi2;
+            //txtSonuc.Text = Convert.ToString(Toplama);
+
+            //MessageBox.Show("Toplama işleminin sonucu= " + Toplama);
+            //MessageBox.Show("Toplama= " + Convert.ToString((txtSayi1.Text + txtSayi2.Text)));
+
+            if (txtSayi1.Text != "" || txtSayi2.Text != "")
+                txtSonuc.Text = (Convert.ToInt32(txtSayi1.Text) + Convert.ToUInt32(txtSayi2.Text)).ToString();
+            else
+                MessageBox.Show("Sayıları girmelisiniz!");
+            
+        }
+    }
+}
