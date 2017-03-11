1100dcf3587d4ca19c082b7c3d3a32596633be5b
HilalSener <crescent.forest@hotmail.com>
2017-03-11 13:39:39 +0300

    New solution added into ComboBox project.

--- WindowsFormApplications/ComboBox/ComboBox/Form1.cs
+++ WindowsFormApplications/ComboBox/ComboBox/Form1.cs
@@ -19,23 +19,21 @@ namespace ComboBox
 
         private void cbPhone_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(cbPhone.SelectedItem.ToString() == "Apple")
+            lbModeller.Items.Clear();
+            if (cbPhone.SelectedItem.ToString() == "Apple")
             {
-                lbModeller.Items.Clear();
                 lbModeller.Items.Add("iPhone 6");
                 lbModeller.Items.Add("iPhone 6S");
                 lbModeller.Items.Add("iPhone 7");
             }
             else if(cbPhone.SelectedItem.ToString() == "Samsung")
             {
-                lbModeller.Items.Clear();
                 lbModeller.Items.Add("Galaxy Note 5");
                 lbModeller.Items.Add("Galaxy S7");
                 lbModeller.Items.Add("Galaxy S7 Edge");
             }
             else if(cbPhone.SelectedItem.ToString() == "Nokia")
             {
-                lbModeller.Items.Clear();
                 lbModeller.Items.Add("Luima 520");
                 lbModeller.Items.Add("Luima 1120");
                 lbModeller.Items.Add("Lumia 1320");
