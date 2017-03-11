ed03502cf6f053a745a0c976c9535616e26dbeb8
HilalSener <crescent.forest@hotmail.com>
2017-03-11 11:09:58 +0300

    LucySeven project added.

---
+++ WindowsFormApplications/LucySeven/LucySeven/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LucySeven
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGame = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(130, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Snow;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(219, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 33);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "7";
+            // 
+            // btnGame
+            // 
+            this.btnGame.Location = new System.Drawing.Point(104, 159);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(75, 40);
+            this.btnGame.TabIndex = 1;
+            this.btnGame.Text = "START";
+            this.btnGame.UseVisualStyleBackColor = true;
+            this.btnGame.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(245, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(219, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGame);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormApplications/LucySeven/LucySeven/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LucySeven
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = rnd.Next(1, 9).ToString();
+            label2.Text = rnd.Next(1, 9).ToString();
+            label3.Text = rnd.Next(1, 9).ToString();
+        }
+
+        int Hak = 10;
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+            if (btnGame.Text == "START")
+            {
+                timer1.Enabled = true;
+                btnGame.Text = "STOP";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btnGame.Text = "START";
+
+                if (label1.Text == label2.Text && label2.Text == label3.Text)
+                {
+                    MessageBox.Show("Tebrikler Kazandın!", "WIN!, WIN!, WIN!");
+                    Hak += 10;
+                    label4.Text = Hak.ToString();
+                }
+                else if(label1.Text == label2.Text || label2.Text == label3.Text || label1.Text == label3.Text)
+                {
+                    MessageBox.Show("Tebrikler 2 tanesini doğru bildin!", "Oynamaya DEVAM!");
+                    Hak += 3;
+                    label4.Text = Hak.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Tekrar denemelisin ;)", "Şimdilik Olmadı...");
+                    Hak--;
+                    label4.Text = Hak.ToString();
+                    if(Hak == 0)
+                    {
+                        MessageBox.Show("Hakkın doldu :(");
+                    }
+                }
+            }
+        }
+    }
+}
