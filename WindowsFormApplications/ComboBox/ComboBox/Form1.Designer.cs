214e6230f8f017c442b9e078b27d4fd504970f56
HilalSener <crescent.forest@hotmail.com>
2017-03-11 13:34:40 +0300

    ComboBox project added.

---
+++ WindowsFormApplications/ComboBox/ComboBox/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPhone = new System.Windows.Forms.ComboBox();
+            this.lbModeller = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cbPhone
+            // 
+            this.cbPhone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPhone.FormattingEnabled = true;
+            this.cbPhone.Items.AddRange(new object[] {
+            "Apple",
+            "Samsung",
+            "Nokia"});
+            this.cbPhone.Location = new System.Drawing.Point(80, 47);
+            this.cbPhone.Name = "cbPhone";
+            this.cbPhone.Size = new System.Drawing.Size(121, 21);
+            this.cbPhone.TabIndex = 0;
+            this.cbPhone.SelectedIndexChanged += new System.EventHandler(this.cbPhone_SelectedIndexChanged);
+            // 
+            // lbModeller
+            // 
+            this.lbModeller.FormattingEnabled = true;
+            this.lbModeller.Location = new System.Drawing.Point(80, 75);
+            this.lbModeller.Name = "lbModeller";
+            this.lbModeller.Size = new System.Drawing.Size(120, 95);
+            this.lbModeller.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbModeller);
+            this.Controls.Add(this.cbPhone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPhone;
+        private System.Windows.Forms.ListBox lbModeller;
+    }
+}
+
---
+++ WindowsFormApplications/ComboBox/ComboBox/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbPhone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbPhone.SelectedItem.ToString() == "Apple")
+            {
+                lbModeller.Items.Clear();
+                lbModeller.Items.Add("iPhone 6");
+                lbModeller.Items.Add("iPhone 6S");
+                lbModeller.Items.Add("iPhone 7");
+            }
+            else if(cbPhone.SelectedItem.ToString() == "Samsung")
+            {
+                lbModeller.Items.Clear();
+                lbModeller.Items.Add("Galaxy Note 5");
+                lbModeller.Items.Add("Galaxy S7");
+                lbModeller.Items.Add("Galaxy S7 Edge");
+            }
+            else if(cbPhone.SelectedItem.ToString() == "Nokia")
+            {
+                lbModeller.Items.Clear();
+                lbModeller.Items.Add("Luima 520");
+                lbModeller.Items.Add("Luima 1120");
+                lbModeller.Items.Add("Lumia 1320");
+            }
+        }
+    }
+}
