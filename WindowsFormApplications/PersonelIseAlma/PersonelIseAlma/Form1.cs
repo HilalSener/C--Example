ab1e8561016e59ef5e9e1daae3d3c7a1cd39f0da
HilalSener <crescent.forest@hotmail.com>
2017-03-12 13:33:27 +0300

    New solution added into PersonelIseAlma project.

--- WindowsFormApplications/PersonelIseAlma/PersonelIseAlma/Form1.cs
+++ WindowsFormApplications/PersonelIseAlma/PersonelIseAlma/Form1.cs
@@ -34,22 +34,30 @@ namespace PersonelIseAlma
             int Yas = DateTime.Now.Year - dtpDogumTarihi.Value.Year;
             if (txtAd.Text.Trim() != "" && txtSoyad.Text.Trim() != "" && txtTelefon.Text != "(   )    -  -")
             {
-                if (Yas < 20 || Yas > 35)
-                    MessageBox.Show("Yaş sebebi işe başvuru yapamazsınız!");
-                else if (cbEgitim.SelectedItem.ToString() == "Lise" || cbEgitim.SelectedItem.ToString() == "Doktora")
-                    MessageBox.Show("Eğitim durumu sebebiyle başvuru yapamazsınız!");
-                else if (cbxIngilizce.Checked != true)
-                    MessageBox.Show("Yabancı dil sebebiyle başvuru yapamazsınız!");
-                else if (cbxRusca.Checked != true && cbxArapca.Checked != true)
-                    MessageBox.Show("Yabancı dil sebebiyle başvuru yapamazsınız!");
-                else if (cbxYazilim.Checked != true || cbxMsOffice.Checked != true)
-                    MessageBox.Show("Bilgisayar bilgisi sebebiyle başvuru yapamasınız!");
-                else if (cbxVeritabani.Checked != true && cbxWebTasarim.Checked != true)
-                    MessageBox.Show("Bilgisayar bilgisi sebebiyle başvuru yapamazsınız!");
-                else if (rbErkek.Checked == true && rbYapilmadi.Checked == true)
-                    MessageBox.Show("Askerlik sebebiyle başvuru yapamazsınız!");
-                else
+                //if (Yas < 20 || Yas > 35)
+                //    MessageBox.Show("Yaş sebebi işe başvuru yapamazsınız!");
+                //else if (cbEgitim.SelectedItem.ToString() == "Lise" || cbEgitim.SelectedItem.ToString() == "Doktora")
+                //    MessageBox.Show("Eğitim durumu sebebiyle başvuru yapamazsınız!");
+                //else if (cbxIngilizce.Checked != true)
+                //    MessageBox.Show("Yabancı dil sebebiyle başvuru yapamazsınız!");
+                //else if (cbxRusca.Checked != true && cbxArapca.Checked != true)
+                //    MessageBox.Show("Yabancı dil sebebiyle başvuru yapamazsınız!");
+                //else if (cbxYazilim.Checked != true || cbxMsOffice.Checked != true)
+                //    MessageBox.Show("Bilgisayar bilgisi sebebiyle başvuru yapamasınız!");
+                //else if (cbxVeritabani.Checked != true && cbxWebTasarim.Checked != true)
+                //    MessageBox.Show("Bilgisayar bilgisi sebebiyle başvuru yapamazsınız!");
+                //else if (rbErkek.Checked == true && rbYapilmadi.Checked == true)
+                //    MessageBox.Show("Askerlik sebebiyle başvuru yapamazsınız!");
+                //else
+                //    MessageBox.Show("Sayın " + txtAd.Text + " " + txtSoyad.Text + " kriterleriniz uygundur, iş başvurunuz dikkate alınmıştır.");
+
+                if ((Yas >= 20 || Yas <= 35) && (cbEgitim.SelectedItem.ToString() != "Lise" || cbEgitim.SelectedItem.ToString() != "Doktora") && cbxIngilizce.Checked == true && (cbxRusca.Checked || cbxArapca.Checked) && (cbxYazilim.Checked && cbxMsOffice.Checked) && (cbxVeritabani.Checked || cbxWebTasarim.Checked) && (rbKadin.Checked || (rbErkek.Checked && rbYapildi.Checked) || (rbErkek.Checked && rbTecilli.Checked)))
+                {
                     MessageBox.Show("Sayın " + txtAd.Text + " " + txtSoyad.Text + " kriterleriniz uygundur, iş başvurunuz dikkate alınmıştır.");
+                }
+                else
+                    MessageBox.Show("Kriterleriniz uygun değil, işe alınmadınız!");
+
             }
             else
                 MessageBox.Show("İsim, soyad ve telefon bilgilerinizi giriniz!");
