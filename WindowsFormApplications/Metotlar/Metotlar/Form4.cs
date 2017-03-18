8123ec361cb83acc589a62ff060ff465f395552e
HilalSener <crescent.forest@hotmail.com>
2017-03-18 14:01:29 +0300

    New solution added into Metotlar project

--- WindowsFormApplications/Metotlar/Metotlar/Form4.cs
+++ WindowsFormApplications/Metotlar/Metotlar/Form4.cs
@@ -19,9 +19,11 @@ namespace Metotlar
 
         private void Form4_Load(object sender, EventArgs e)
         {
-            int a = 5; //Stack hafıza value type
+            int a = 5; //Value type; Stack hafızada tutuluyor.
             MessageBox.Show(a.ToString());
-            DegerAl(a);
+            //DegerAl(a);
+            //DegerAl(ref a);
+            DegerAl(out a);
             MessageBox.Show(a.ToString());
 
             //string Sehir = "Izmir";
@@ -29,13 +31,27 @@ namespace Metotlar
             //ReferansAl(Sehir);
             //MessageBox.Show(Sehir.ToString());
 
-            string[] Sehirler = { "İzmir", "Ankara", "Bursa", "Adana"};
+            string[] Sehirler = { "İzmir", "Ankara", "Bursa", "Adana"}; //Referans type; "string[], object" heap hafızada tutuluyor.
             MessageBox.Show(Sehirler[0]);
             ReferansAl(Sehirler);
             MessageBox.Show(Sehirler[0]);
+
+            //Değer tipli değişkenlerin içindekiler bit bit metotlara aktarılırken referans tipli değişkenlerin adresleri aktarılır. Bu nedenle value type değişkenlerin kendi deeğerleri metot içindeki eğişimlerden etkinlenmezken, referans tipli(string dizi, object) değişkenlerin adresleri aktarıldığından metotlardaki değişiklikler de aynı adres üzerinde gerçekleşir ve değişkenlerin orjinal değerleri de değişir 
+            //Eğer valuse type değişkenlerin de adresleri dikkate alınmak istenirse önlerine ref eki getirilmelidir. Ancak bu durumda da kullanılmadan önce value type değişkene mutlaka bir ilk değer atanmış olmalıdır. 
+            //Eğer "ref" yerine "out" ön eki kullanılırsa ilk değer verilmeden de value type değişkenlerin adresleri referans type değişkenlerdeli gibi işleme sokulur.
         }
 
-        private void DegerAl(int a)
+        //private void DegerAl(int a)
+        //{
+        //    a = 10;
+        //}
+
+        //private void DegerAl(ref int a)
+        //{
+        //    a = 10;
+        //}
+
+        private void DegerAl(out int a)
         {
             a = 10;
         }
