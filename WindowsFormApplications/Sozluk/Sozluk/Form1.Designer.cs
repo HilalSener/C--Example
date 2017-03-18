c4325cdd17c14400d339b4638846fa472c8fb920
HilalSener <crescent.forest@hotmail.com>
2017-03-18 09:16:45 +0300

    #fix

--- WindowsFormApplications/Sozluk/Sozluk/Form1.Designer.cs
+++ WindowsFormApplications/Sozluk/Sozluk/Form1.Designer.cs
@@ -44,49 +44,44 @@
             // 
             this.lblTurkce.AutoSize = true;
             this.lblTurkce.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblTurkce.Location = new System.Drawing.Point(156, 128);
-            this.lblTurkce.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTurkce.Location = new System.Drawing.Point(117, 104);
             this.lblTurkce.Name = "lblTurkce";
-            this.lblTurkce.Size = new System.Drawing.Size(60, 20);
+            this.lblTurkce.Size = new System.Drawing.Size(52, 17);
             this.lblTurkce.TabIndex = 0;
             this.lblTurkce.Text = "Türkçe";
             // 
             // txtTurkce
             // 
-            this.txtTurkce.Location = new System.Drawing.Point(233, 128);
-            this.txtTurkce.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTurkce.Location = new System.Drawing.Point(175, 104);
             this.txtTurkce.Name = "txtTurkce";
             this.txtTurkce.ReadOnly = true;
-            this.txtTurkce.Size = new System.Drawing.Size(167, 22);
+            this.txtTurkce.Size = new System.Drawing.Size(126, 20);
             this.txtTurkce.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(156, 169);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(117, 137);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.Size = new System.Drawing.Size(58, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "İngilizce";
             // 
             // txtIngilizce
             // 
-            this.txtIngilizce.Location = new System.Drawing.Point(233, 169);
-            this.txtIngilizce.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIngilizce.Location = new System.Drawing.Point(175, 137);
             this.txtIngilizce.Name = "txtIngilizce";
             this.txtIngilizce.ReadOnly = true;
-            this.txtIngilizce.Size = new System.Drawing.Size(167, 22);
+            this.txtIngilizce.Size = new System.Drawing.Size(126, 20);
             this.txtIngilizce.TabIndex = 2;
             // 
             // btnBul
             // 
             this.btnBul.BackColor = System.Drawing.Color.Snow;
-            this.btnBul.Location = new System.Drawing.Point(411, 128);
-            this.btnBul.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBul.Location = new System.Drawing.Point(308, 104);
             this.btnBul.Name = "btnBul";
-            this.btnBul.Size = new System.Drawing.Size(100, 65);
+            this.btnBul.Size = new System.Drawing.Size(75, 53);
             this.btnBul.TabIndex = 3;
             this.btnBul.Text = "Bul";
             this.btnBul.UseVisualStyleBackColor = false;
@@ -95,10 +90,9 @@
             // rbTrToIng
             // 
             this.rbTrToIng.AutoSize = true;
-            this.rbTrToIng.Location = new System.Drawing.Point(16, 34);
-            this.rbTrToIng.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbTrToIng.Location = new System.Drawing.Point(12, 28);
             this.rbTrToIng.Name = "rbTrToIng";
-            this.rbTrToIng.Size = new System.Drawing.Size(136, 21);
+            this.rbTrToIng.Size = new System.Drawing.Size(106, 17);
             this.rbTrToIng.TabIndex = 4;
             this.rbTrToIng.Text = "Türkçe - İngilizce";
             this.rbTrToIng.UseVisualStyleBackColor = true;
@@ -107,10 +101,9 @@
             // rbIngtToTr
             // 
             this.rbIngtToTr.AutoSize = true;
-            this.rbIngtToTr.Location = new System.Drawing.Point(372, 34);
-            this.rbIngtToTr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbIngtToTr.Location = new System.Drawing.Point(279, 28);
             this.rbIngtToTr.Name = "rbIngtToTr";
-            this.rbIngtToTr.Size = new System.Drawing.Size(136, 21);
+            this.rbIngtToTr.Size = new System.Drawing.Size(106, 17);
             this.rbIngtToTr.TabIndex = 4;
             this.rbIngtToTr.Text = "İngilizce - Türkçe";
             this.rbIngtToTr.UseVisualStyleBackColor = true;
@@ -119,18 +112,18 @@
             // lbKelimeler
             // 
             this.lbKelimeler.FormattingEnabled = true;
-            this.lbKelimeler.ItemHeight = 16;
-            this.lbKelimeler.Location = new System.Drawing.Point(16, 105);
-            this.lbKelimeler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbKelimeler.Location = new System.Drawing.Point(12, 85);
             this.lbKelimeler.Name = "lbKelimeler";
-            this.lbKelimeler.Size = new System.Drawing.Size(125, 228);
+            this.lbKelimeler.Size = new System.Drawing.Size(95, 186);
             this.lbKelimeler.TabIndex = 5;
             this.lbKelimeler.DoubleClick += new System.EventHandler(this.lbKelimeler_DoubleClick);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(160, 223);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(117, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 17);
             this.label2.TabIndex = 6;
@@ -138,19 +131,18 @@
             // 
             // txtCeviri
             // 
-            this.txtCeviri.Location = new System.Drawing.Point(233, 220);
-            this.txtCeviri.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCeviri.Location = new System.Drawing.Point(175, 179);
             this.txtCeviri.Name = "txtCeviri";
             this.txtCeviri.ReadOnly = true;
-            this.txtCeviri.Size = new System.Drawing.Size(167, 22);
+            this.txtCeviri.Size = new System.Drawing.Size(126, 20);
             this.txtCeviri.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(540, 406);
+            this.ClientSize = new System.Drawing.Size(405, 330);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbKelimeler);
             this.Controls.Add(this.rbIngtToTr);
@@ -161,7 +153,6 @@
             this.Controls.Add(this.txtTurkce);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTurkce);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
