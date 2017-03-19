f7b6fe7eba36114f5f2fb637a3dca39e019607ac
Hilal Sener <crescent.forest@hotmail.com>
2017-03-19 14:35:49 +0300

    Streams project added.

---
+++ WindowsFormApplications/Streams/Streams/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using  System.IO;
+
+namespace Streams
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Stream (Akım - Text dosyasına yazma - okuma I/O işlemleri için System.IO kütüphanesi - namespace eklenmelidir.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //FileStream fs = new FileStream("Ornek.txt", FileMode.OpenOrCreate);
+            //StreamWriter sw = new StreamWriter(fs);
+            //StreamWriter sw = new StreamWriter("Ornek.txt"); // Dosyayı her seferinde yeniden oluşturup baştan yazar.
+            StreamWriter sw = new StreamWriter("Ornek.txt", true); //Dosyayı kayıt ekleme amacıyla açar.
+            sw.Write("1.Satır");
+            sw.Write("2.Satır");
+            sw.WriteLine("3.Satır");
+            sw.WriteLine("4.Satır");
+
+            sw.Close();
+
+            StreamReader sr = new StreamReader("Ornek.txt");
+            MessageBox.Show(sr.ReadToEnd());
+
+            sr.Close();
+        }
+    }
+}
---
+++ WindowsFormApplications/Streams/Streams/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Streams
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            StreamWriter DosyaYaz = new StreamWriter("Metin.txt");
+            DosyaYaz.WriteLine(txtSatir.Text);
+
+            DosyaYaz.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            StreamWriter DosyaEkle = new StreamWriter("Metin.txt", true);
+            DosyaEkle.WriteLine(txtSatir.Text);
+
+            DosyaEkle.Close();
+        }
+
+        private void btnOku_Click(object sender, EventArgs e)
+        {
+            StreamReader DosyaOku = new StreamReader("Metin.txt");
+            rbtxtMetin.Text = DosyaOku.ReadLine(); //1 satır okur.
+
+            DosyaOku.Close();
+        }
+
+        private void btnHepsiniOku_Click(object sender, EventArgs e)
+        {
+            StreamReader DosyaOku = new StreamReader("Metin.txt");
+            rbtxtMetin.Text = DosyaOku.ReadToEnd(); //Sona kadar hepsini okur.
+            //ReadToEnd yerine ReadLine kullanmayı deneyelim. İpucu, "null" ile kontrol et!
+
+            DosyaOku.Close();
+        }
+    }
+}
