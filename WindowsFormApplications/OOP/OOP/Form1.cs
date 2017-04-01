e706adb14f24b59712520f5234ee1bf6448272a9
HilalSener <crescent.forest@hotmail.com>
2017-04-01 14:34:23 +0300

    OOP project added.

---
+++ WindowsFormApplications/OOP/OOP/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Telefon tel1 = new Telefon();
+            tel1.Marka = "Samsung";
+            tel1.Model = "Galaxy S7";
+            tel1.Fiyat = 3000;
+            MessageBox.Show(tel1.Marka + ","  + tel1.Model + " Fiyatı :" + tel1.Fiyat);
+        }
+    }
+
+    class Telefon
+    {
+        public string Marka;
+        public string Model;
+        public string Islemci;
+        public string Hafiza;
+        public string Kamera;
+        public string Ekran;
+        public double Fiyat;
+    }
+}
---
+++ WindowsFormApplications/OOP/OOP/cTelefon.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class cTelefon   //class cTelefon bu projeden her yerde görülebilir.
+    {
+        #region Members
+        private string _marka;
+        private string _model;
+        private string _islemci;
+        private string _hafiza;
+        private string _kamera;
+        private string _ekran;
+        private double _fiyat; 
+        #endregion
+
+        #region Properties
+        public string Marka
+        {
+            get
+            {
+                return _marka;
+            }
+
+            set
+            {
+                _marka = value.Substring(0, 1).ToUpper() + value.Substring(1).ToLower();
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+
+            set
+            {
+                _model = value;
+            }
+        }
+
+        public string Islemci
+        {
+            get
+            {
+                return _islemci;
+            }
+
+            set
+            {
+                _islemci = value;
+            }
+        }
+
+        public string Hafiza
+        {
+            get
+            {
+                return _hafiza;
+            }
+
+            set
+            {
+                _hafiza = value;
+            }
+        }
+
+        public string Kamera
+        {
+            get
+            {
+                return _kamera;
+            }
+
+            set
+            {
+                _kamera = value;
+            }
+        }
+
+        public string Ekran
+        {
+            get
+            {
+                return _ekran;
+            }
+
+            set
+            {
+                _ekran = value;
+            }
+        }
+
+        public double Fiyat
+        {
+            get
+            {
+                return _fiyat;
+            }
+
+            set
+            {
+                _fiyat = value;
+            }
+        }
+        #endregion
+
+
+    }
+}
