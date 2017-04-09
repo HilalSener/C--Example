2255382c969c219f57d5bf77e600851e4719b071
HilalSener <crescent.forest@hotmail.com>
2017-04-09 11:11:17 +0300

    wf-kutuphane project added.

---
+++ WindowsFormApplications/wf-kutuphane/wf-kutuphane/cKitapTuru.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf_kutuphane
+{
+    public class cKitapTuru
+    {
+        private int _id;
+        private string _kitapTuru;
+
+        #region Properties
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string KitapTuru
+        {
+            get
+            {
+                return _kitapTuru;
+            }
+
+            set
+            {
+                _kitapTuru = value;
+            }
+        }
+
+        public void KitapTurleriGoster(ListBox liste)
+        {
+            liste.Items.Clear();
+            StreamReader DosyaOku = new StreamReader("KitapTurleri.txt");
+            string Okunan = DosyaOku.ReadLine();
+            while(Okunan != null)
+            {
+                string[] Degerler = Okunan.Split(';');
+                liste.Items.Add(Degerler[1]);
+                Okunan = DosyaOku.ReadLine();
+            }
+            DosyaOku.Close();
+        }
+
+        public bool KitapTuruEkle(cKitapTuru kt)
+        {
+            StreamWriter DosyaYaz = new StreamWriter("KitapTurleri.txt", true);
+            DosyaYaz.WriteLine(kt._id  + ";" + kt._kitapTuru);
+            DosyaYaz.Close();
+            return true;
+        }
+
+        public bool KitapTuruVarmi(string KitapTuru)
+        {
+            StreamReader DosyaOku = new StreamReader("KitapTurleri.txt");
+            string Okunan = DosyaOku.ReadLine();
+            while(Okunan != null)
+            {
+                string[] Degerler = Okunan.Split(';');
+                if (Degerler[1] == KitapTuru)
+                {
+                    DosyaOku.Close();
+                    return true;
+                }
+                Okunan = DosyaOku.ReadLine();
+            }
+            DosyaOku.Close();
+            return false;
+        }
+        #endregion
+
+        public int SonIDBul()
+        {
+            int id = 0;
+            StreamWriter DosyaOlustur = new StreamWriter("KitapTurleri.txt", true);
+            DosyaOlustur.Close();
+
+            StreamReader DosyaOku = new StreamReader("KitapTurleri.txt");
+            string Okunan = DosyaOku.ReadLine();
+            if (Okunan == null)
+                id = 1;
+            else
+                while(Okunan != null)
+                {
+                    string[] Degerler = Okunan.Split(';');
+                    id = (Convert.ToInt32(Degerler[0]) + 1);
+                    Okunan = DosyaOku.ReadLine();
+                }
+            DosyaOku.Close();
+            return id;
+        }
+    }
+}
---
+++ WindowsFormApplications/wf-kutuphane/wf-kutuphane/frmKitapTurleri.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace wf_kutuphane
+{
+    partial class frmKitapTurleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKitapTuru = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbKitapTurleri = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap Tür ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(149, 63);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(63, 23);
+            this.txtID.TabIndex = 1;
+            this.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kitap Türü";
+            // 
+            // txtKitapTuru
+            // 
+            this.txtKitapTuru.Location = new System.Drawing.Point(149, 122);
+            this.txtKitapTuru.Name = "txtKitapTuru";
+            this.txtKitapTuru.Size = new System.Drawing.Size(183, 23);
+            this.txtKitapTuru.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(149, 171);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(81, 35);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbKitapTurleri
+            // 
+            this.lbKitapTurleri.FormattingEnabled = true;
+            this.lbKitapTurleri.ItemHeight = 16;
+            this.lbKitapTurleri.Location = new System.Drawing.Point(43, 255);
+            this.lbKitapTurleri.Name = "lbKitapTurleri";
+            this.lbKitapTurleri.Size = new System.Drawing.Size(359, 180);
+            this.lbKitapTurleri.TabIndex = 5;
+            // 
+            // frmKitapTurleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(444, 508);
+            this.Controls.Add(this.lbKitapTurleri);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtKitapTuru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmKitapTurleri";
+            this.Text = "Kitap Türleri";
+            this.Load += new System.EventHandler(this.frmKitapTurleri_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKitapTuru;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbKitapTurleri;
+    }
+}
