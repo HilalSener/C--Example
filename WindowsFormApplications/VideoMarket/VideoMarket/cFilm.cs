6270903e2d2674fcda537aa10d5e40ff520d62b9
HilalSener <crescent.forest@hotmail.com>
2017-05-14 10:35:44 +0300

    New solution added into VideoMarket project

---
+++ WindowsFormApplications/VideoMarket/VideoMarket/cFilm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoMarket
+{
+    public class cFilm
+    {
+        private int _filmNo;
+        private string _filmAd;
+        private int _filmTurNo;
+        private string _yonetmen;
+        private string _oyuncular;
+        private string _ozet;
+        private int _miktar;
+        private int _fiyat;
+
+        #region Properties
+        public int FilmNo
+        {
+            get
+            {
+                return _filmNo;
+            }
+
+            set
+            {
+                _filmNo = value;
+            }
+        }
+
+        public string FilmAd
+        {
+            get
+            {
+                return _filmAd;
+            }
+
+            set
+            {
+                _filmAd = value;
+            }
+        }
+
+        public int FilmTurNo
+        {
+            get
+            {
+                return _filmTurNo;
+            }
+
+            set
+            {
+                _filmTurNo = value;
+            }
+        }
+
+        public string Yonetmen
+        {
+            get
+            {
+                return _yonetmen;
+            }
+
+            set
+            {
+                _yonetmen = value;
+            }
+        }
+
+        public string Oyuncular
+        {
+            get
+            {
+                return _oyuncular;
+            }
+
+            set
+            {
+                _oyuncular = value;
+            }
+        }
+
+        public string Ozet
+        {
+            get
+            {
+                return _ozet;
+            }
+
+            set
+            {
+                _ozet = value;
+            }
+        }
+
+        public int Miktar
+        {
+            get
+            {
+                return _miktar;
+            }
+
+            set
+            {
+                _miktar = value;
+            }
+        }
+
+        public int Fiyat
+        {
+            get
+            {
+                return _fiyat;
+            }
+
+            set
+            {
+                _fiyat = value;
+            }
+        }
+        #endregion
+
+        SqlConnection conn = new SqlConnection(cGenel.connStr);
+
+
+    }
+}
---
+++ WindowsFormApplications/VideoMarket/VideoMarket/frmFilmler.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace VideoMarket
+{
+    partial class frmFilmler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFilmNo = new System.Windows.Forms.TextBox();
+            this.lvFilmler = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnDegistir = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.txtOzet = new System.Windows.Forms.TextBox();
+            this.txtOyuncular = new System.Windows.Forms.TextBox();
+            this.txtYonetmen = new System.Windows.Forms.TextBox();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.txtFilmAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbFilmTuru = new System.Windows.Forms.ComboBox();
+            this.txtFilmTuru = new System.Windows.Forms.TextBox();
+            this.txtTurNo = new System.Windows.Forms.TextBox();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 17);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Film Adına Göre Arama";
+            // 
+            // txtFilmNo
+            // 
+            this.txtFilmNo.Location = new System.Drawing.Point(31, 112);
+            this.txtFilmNo.Name = "txtFilmNo";
+            this.txtFilmNo.ReadOnly = true;
+            this.txtFilmNo.Size = new System.Drawing.Size(36, 22);
+            this.txtFilmNo.TabIndex = 34;
+            // 
+            // lvFilmler
+            // 
+            this.lvFilmler.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lvFilmler.FullRowSelect = true;
+            this.lvFilmler.Location = new System.Drawing.Point(470, 114);
+            this.lvFilmler.Name = "lvFilmler";
+            this.lvFilmler.Size = new System.Drawing.Size(390, 323);
+            this.lvFilmler.TabIndex = 25;
+            this.lvFilmler.UseCompatibleStateImageBehavior = false;
+            this.lvFilmler.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Film No";
+            this.columnHeader1.Width = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Film Adı";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Film Tur No";
+            this.columnHeader3.Width = 0;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Enabled = false;
+            this.btnSil.Location = new System.Drawing.Point(347, 546);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 30;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnDegistir
+            // 
+            this.btnDegistir.Enabled = false;
+            this.btnDegistir.Location = new System.Drawing.Point(258, 547);
+            this.btnDegistir.Name = "btnDegistir";
+            this.btnDegistir.Size = new System.Drawing.Size(75, 23);
+            this.btnDegistir.TabIndex = 28;
+            this.btnDegistir.Text = "Değiştir";
+            this.btnDegistir.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Enabled = false;
+            this.btnKaydet.Location = new System.Drawing.Point(167, 546);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 27;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(76, 546);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(75, 23);
+            this.btnYeni.TabIndex = 26;
+            this.btnYeni.Text = "Yeni";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            // 
+            // txtOzet
+            // 
+            this.txtOzet.Location = new System.Drawing.Point(180, 321);
+            this.txtOzet.Multiline = true;
+            this.txtOzet.Name = "txtOzet";
+            this.txtOzet.Size = new System.Drawing.Size(245, 116);
+            this.txtOzet.TabIndex = 24;
+            // 
+            // txtOyuncular
+            // 
+            this.txtOyuncular.Location = new System.Drawing.Point(180, 228);
+            this.txtOyuncular.Multiline = true;
+            this.txtOyuncular.Name = "txtOyuncular";
+            this.txtOyuncular.Size = new System.Drawing.Size(245, 69);
+            this.txtOyuncular.TabIndex = 23;
+            // 
+            // txtYonetmen
+            // 
+            this.txtYonetmen.Location = new System.Drawing.Point(180, 189);
+            this.txtYonetmen.Name = "txtYonetmen";
+            this.txtYonetmen.Size = new System.Drawing.Size(245, 22);
+            this.txtYonetmen.TabIndex = 22;
+            // 
+            // txtArama
+            // 
+            this.txtArama.Location = new System.Drawing.Point(180, 71);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(245, 22);
+            this.txtArama.TabIndex = 20;
+            // 
+            // txtFilmAdi
+            // 
+            this.txtFilmAdi.Location = new System.Drawing.Point(180, 114);
+            this.txtFilmAdi.Name = "txtFilmAdi";
+            this.txtFilmAdi.Size = new System.Drawing.Size(245, 22);
+            this.txtFilmAdi.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Film Özeti";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Oyuncular";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Yönetmen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Film Adı";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Film Türü";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Yönetmen";
+            this.columnHeader5.Width = 150;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Oyuncular";
+            this.columnHeader6.Width = 150;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Özet";
+            this.columnHeader7.Width = 150;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Miktar";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Fiyat";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 17);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Film Türü";
+            // 
+            // cbFilmTuru
+            // 
+            this.cbFilmTuru.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilmTuru.FormattingEnabled = true;
+            this.cbFilmTuru.Location = new System.Drawing.Point(403, 153);
+            this.cbFilmTuru.Name = "cbFilmTuru";
+            this.cbFilmTuru.Size = new System.Drawing.Size(22, 24);
+            this.cbFilmTuru.TabIndex = 36;
+            // 
+            // txtFilmTuru
+            // 
+            this.txtFilmTuru.Location = new System.Drawing.Point(180, 154);
+            this.txtFilmTuru.Name = "txtFilmTuru";
+            this.txtFilmTuru.ReadOnly = true;
+            this.txtFilmTuru.Size = new System.Drawing.Size(217, 22);
+            this.txtFilmTuru.TabIndex = 22;
+            // 
+            // txtTurNo
+            // 
+            this.txtTurNo.Location = new System.Drawing.Point(31, 151);
+            this.txtTurNo.Name = "txtTurNo";
+            this.txtTurNo.ReadOnly = true;
+            this.txtTurNo.Size = new System.Drawing.Size(36, 22);
+            this.txtTurNo.TabIndex = 34;
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(180, 456);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(111, 22);
+            this.txtMiktar.TabIndex = 37;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 456);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "Miktar";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(180, 496);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(111, 22);
+            this.txtFiyat.TabIndex = 39;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(73, 496);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 17);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Fiyat";
+            // 
+            // frmFilmler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(916, 636);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtMiktar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbFilmTuru);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTurNo);
+            this.Controls.Add(this.txtFilmNo);
+            this.Controls.Add(this.lvFilmler);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnDegistir);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.txtOzet);
+            this.Controls.Add(this.txtOyuncular);
+            this.Controls.Add(this.txtFilmTuru);
+            this.Controls.Add(this.txtYonetmen);
+            this.Controls.Add(this.txtArama);
+            this.Controls.Add(this.txtFilmAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFilmler";
+            this.Text = "Film Tanımlama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFilmNo;
+        private System.Windows.Forms.ListView lvFilmler;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnDegistir;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.TextBox txtOzet;
+        private System.Windows.Forms.TextBox txtOyuncular;
+        private System.Windows.Forms.TextBox txtYonetmen;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.TextBox txtFilmAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbFilmTuru;
+        private System.Windows.Forms.TextBox txtFilmTuru;
+        private System.Windows.Forms.TextBox txtTurNo;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label8;
+    }
+}
